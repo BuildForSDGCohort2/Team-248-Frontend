f1a5d2a2722c96374d6f730b27a5bfb53f92c6c3
AbdullahMuhammed <abdo56668@gmail.com>
2020-08-24 23:30:05 +0200

    add form validation

--- src/containers/SignUp/SignUp.js
+++ src/containers/SignUp/SignUp.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Copyright from '../../components/CopyRight'
 import './SignUp.scss'
@@ -18,154 +15,255 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+export default class SignUp extends React.Component {
 
+  state = {
+    name: {
+      value: '',
+      error: '',
+    },
+    email: {
+      value: '',
+      error: '',
+    },
+    password: {
+      value: '',
+      error: ''
+    },
+    password_confirmation: {
+      value: '',
+      error: ''
+    },
+    phone: {
+      value: '',
+      error: ''
+    },
+    address: {
+      value: '',
+      error: ''
+    },
+    date_of_birth: {
+      value: '',
+      error: ''
+    },
+  }
 
-const today = new Date();
-const date = `${today.getFullYear()}-${(today.getMonth()+1)}-${today.getDate()}`;
+  handleChange = (event) => {
+    const required = event.target.value !== ""
+    this.setState(
+      {
+        [event.target.name]: {
+          value: event.target.value,
+          error: (!required && "This feild is required"),
+        }
+      })
+  }
 
-export default function SignUp() {
-  const classes = useStyles();
-  const [genderValue, setValue] = React.useState('female');
+  handleEmailChange = (event) => {
+    const validEmail = /\S+@\S+\.\S+/.test(event.target.value);
+    const required = event.target.value !== ""
+    this.setState(
+      {
+        email: {
+          value: event.target.value,
+          error: (!required && "This field is required")
+          || (!validEmail && "Not valid"),
+        }
+      })
+  }
 
-  const handleChangeGender = (event) => {
-    setValue(event.target.value);
-  };
+  handlePasswordChange = (event) => {
+    const min = event.target.value.length >= 6
+    const max = event.target.value.length <= 12
+    this.setState(
+      {
+        password: {
+          value: event.target.value,
+          error: ((!min || !max) 
+          && "This field should be 6 minimum charachter and 12 maximum charachter")        }
+      })
+  }
 
-  return (
-    <Container component="main" maxWidth="xs">
-      {/* <CssBaseline /> */}
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                autoComplete="name"
-                name="name"
-                variant="outlined"
-                required
-                fullWidth
-                id="name"
-                label="Your Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                name="password_confirmation"
-                label="Confirm Password"
-                type="password"
-                id="password_confirmation"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="phone"
-                label="Phone Number"
-                name="phone"
-                autoComplete="phone"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="address"
-                label="Your address"
-                name="address"
-                autoComplete="address"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormLabel component="legend">Date of Birth</FormLabel>
-              <TextField
-                id="date_of_birth"
-                name="date_of_birth"
-                type="date"
-                fullWidth
-                InputLabelProps={{
-                  shrink: true,
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-            <FormControl component="fieldset">
-              <FormLabel component="legend">Gender</FormLabel>
-              <RadioGroup aria-label="gender" name="gender" value={genderValue} onChange={handleChangeGender}>
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="female" control={<Radio />} label="Female" />
-              </RadioGroup>
-            </FormControl>
-            </Grid>
-            
-          </Grid>
+  handleConfirmPassword = (event) =>{
+    const matched = event.target.value === this.state.password.value && event.target.value !== ""
+    this.setState(
+      {
+        password_confirmation: {
+          value: event.target.value,
+          error: (!matched && "Password doesn't match")        }
+      })
+  }
 
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+  handlePhoneNumberChange = (event) =>{
+    const validPhone = /^[0-9]*$/.test(event.target.value)
+    this.setState(
+      {
+        phone: {
+          value: event.target.value,
+          error: (!validPhone && "Invalid Phone number")        }
+      })
+  }
+
+  validate = (event) => {
+    this.handleChange({ target: { 
+      value: event.target.name.value, 
+      name:  event.target.name.name
+    } })
+    this.handleEmailChange({ target: { 
+      value: event.target.email.value, 
+    } })
+    this.handlePasswordChange({ target: { 
+      value: event.target.password.value, 
+    } })
+    this.handleConfirmPassword({ target: { 
+      value: event.target.password_confirmation.value, 
+    } })
+    this.handleChange({ target: { 
+      value: event.target.phone.value,
+      name: event.target.phone.name
+    } })
+    this.handleChange({ target: { 
+      value: event.target.address.value, 
+      name: event.target.address.name
+    } })
+  }
+
+  handleSubmission = (event) => {
+    event.preventDefault()
+    this.validate(event)
+    // console.log(event.target.email.value)
+    // console.log(this.state)
+  }
+
+  render(){
+    return (
+      <Container component="main" maxWidth="xs">
+        <div className="paper">
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form onSubmit={this.handleSubmission}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  autoComplete="name"
+                  name="name"
+                  variant="outlined"
+                  onChange={this.handleChange}
+                  fullWidth
+                  id="name"
+                  label="Your Name"
+                  autoFocus
+                />
+                <small className="error d-block">{this.state.name.error}</small>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  onChange={this.handleEmailChange}
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+                <small className="error d-block">{this.state.email.error}</small>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  onChange={this.handlePasswordChange}
+                  id="password"
+                  type="password"
+                  label="Password"
+                  name="password"
+                  autoComplete="current-password"
+                />
+                <small className="error d-block">{this.state.password.error}</small>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  name="password_confirmation"
+                  onChange={this.handleConfirmPassword}
+                  label="Confirm Password"
+                  type="password"
+                  id="password_confirmation"
+                />
+                <small className="error d-block">{this.state.password_confirmation.error}</small>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  onChange={this.handlePhoneNumberChange}
+                  fullWidth
+                  id="phone"
+                  label="Phone Number"
+                  name="phone"
+                  autoComplete="phone"
+                />
+                <small className="error d-block">{this.state.phone.error}</small>
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="address"
+                  onChange={this.handleChange}
+                  label="Your address"
+                  name="address"
+                  autoComplete="address"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormLabel component="legend">Date of Birth</FormLabel>
+                <TextField
+                  id="date_of_birth"
+                  name="date_of_birth"
+                  type="date"
+                  onChange={this.handleChange}
+                  fullWidth
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+              <FormControl component="fieldset">
+                <FormLabel component="legend">Gender</FormLabel>
+                <RadioGroup aria-label="gender" name="gender" value="male">
+                  <FormControlLabel value="male" control={<Radio />} label="Male" />
+                  <FormControlLabel value="female" control={<Radio />} label="Female" />
+                </RadioGroup>
+              </FormControl>
+              </Grid>
+              
             </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={5}>
-        <Copyright />
-      </Box>
-    </Container>
+  
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className=""
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={5}>
+          <Copyright />
+        </Box>
+      </Container>
   );
+  }
 }
