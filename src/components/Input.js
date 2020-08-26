a439009080a0a7fac66caa4deeb1a5b6b80598a3
AbdullahMuhammed <abdo56668@gmail.com>
2020-08-26 19:35:30 +0200

    fixes for Codacy

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import TextField from '@material-ui/core/TextField';
+import React from "react"
+import TextField from "@material-ui/core/TextField";
 
 
 export const Input = ({variant, margin, handleChange, id, label, name, autoComplete }) => {
