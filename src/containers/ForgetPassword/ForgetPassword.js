c9a5bbeec3d69d5e72ab5eda3e66d1f0f287d7bf
AbdullahMuhammed <abdo56668@gmail.com>
2020-08-27 11:53:21 +0200

    fixes for Codacy 7

--- src/containers/ForgetPassword/ForgetPassword.js
+++ src/containers/ForgetPassword/ForgetPassword.js
@@ -61,7 +61,6 @@ export default class ForgetPassword extends React.Component{
   render() {
     return (
       <Container component="main" maxWidth="xs" className="forget-password-container">
-        <CssBaseline />
         <div className="paper">
           <Typography component="h1" variant="h5">
             Forget Password
