3720657e836650b8f47f2e4a2e5d84c4a57890f6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-01 09:42:24 +0200

    add SecondSection component

---
+++ src/containers/SecondSection/SecondSection.js
@@ -0,0 +1,12 @@
+// The header with navs and router go here (First section)
+import React from "react";
+
+const SecondSection = () => {
+  return (
+    <div>
+      <h3>SecondSection goes here</h3>
+    </div>
+  );
+};
+
+export default SecondSection;
