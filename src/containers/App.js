1a5de79a97690935d539bd74382543b3d1705b05
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-01 09:44:05 +0200

    update components structure

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
-import { Router } from "react-router-dom";
-import history from "../routes/History";
-import Routes from "../routes/Routes";
+//import { Router } from "react-router-dom";
+//import history from "../routes/History";
+//import Routes from "../routes/Routes";
 import "./App.scss";
+import Home from "./Home/Home";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Router history={history}>{Routes}</Router>
-      </div>
+      <>
+        <Home />
+      </>
     );
   }
 }
