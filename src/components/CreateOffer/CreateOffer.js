c9766d780f92e1cb24a509d47db12f0ae9f72d64
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-09-02 18:52:58 +0200

    create offer initial state

--- src/components/CreateOffer/CreateOffer.js
+++ src/components/CreateOffer/CreateOffer.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 
-const CreateOffer = () => {
-  return <h1>Hello Create Offer</h1>;
-};
+class CreateOffer extends Component {
+  state = {
+    startDate: {
+      value: "",
+      error: "Invalid, please try again"
+    },
+    endDate: {
+      value: "",
+      error: "Invalid, please try again"
+    },
+    pricePerHour: {
+      value: "",
+      error: "Invalid, please try again"
+    },
+    address: {
+      value: "",
+      error: "Invalid, please try again"
+    },
+    qualifications: {
+      value: "",
+      error: "Invalid, please try again"
+    },
+    snackbar: {
+      open: false,
+      vertical: "top",
+      horizontal: "center"
+    }
+  };
+  render() {
+    return <h1>Hello Create Offer</h1>;
+  }
+}
 
 export default CreateOffer;
