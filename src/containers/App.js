5de9e82c00eb379172eabc5d02e30d84d1f76b26
AbdullahMuhammed <abdo56668@gmail.com>
2020-09-06 14:59:46 +0200

    fix build issue

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import { Router } from "react-router-dom";
 import history from "../routes/History";
 import Routes from "../routes/Routes";
 import "./App.scss";
-import Home from "./Home/Home";
+
 class App extends Component {
 
   render() {         
