8f767181b0798d71313ca6fc122789f33f5df750
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-14 21:48:52 +0200

    Add Testimonial section

---
+++ src/containers/Home/components/sections/Testimonial.js
@@ -0,0 +1,140 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionTilesProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+
+const propTypes = {
+  ...SectionTilesProps.types,
+};
+
+const defaultProps = {
+  ...SectionTilesProps.defaults,
+};
+
+const Testimonial = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  pushLeft,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "testimonial section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "testimonial-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const tilesClasses = classNames("tiles-wrap", pushLeft && "push-left");
+
+  const sectionHeader = {
+    title: "BabeJo testimonials",
+    paragraph: "Show some famous quotes about blockchain from real experts.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={tilesClasses}>
+            <div
+              className="tiles-item reveal-from-right"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    A lot of people automatically dismiss e-currency as a lost
+                    cause because of all the companies that failed since the
+                    1990's. I hope it's obvious it was only the centrally
+                    controlled nature of those systems that doomed them. I think
+                    this is the first time we're trying a decentralized,
+                    non-trust-based system.
+                  </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">
+                    Satoshi Nakamoto
+                  </span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">Founder of Bitcoin</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="tiles-item reveal-from-bottom">
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    A smart contract is a mechanism involving digital assets and
+                    two or more parties, where some or all of the parties put
+                    assets in, and assets are automatically redistributed among
+                    those parties according to a formula based on certain data
+                    that is not known at the time the contract is initiated.
+                  </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">
+                    Vitalik Buterin
+                  </span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">Founder of Ethereum</a>
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div
+              className="tiles-item reveal-from-left"
+              data-reveal-delay="200"
+            >
+              <div className="tiles-item-inner">
+                <div className="testimonial-item-content">
+                  <p className="text-sm mb-0">
+                    The Ethereum network is a second evolution of the blockchain
+                    concept. It takes the traditional blockchain structure and
+                    adds a programming language that is built inside of it. Each
+                    node contains a complete record of all the transactions that
+                    were ever recorded in that blockchain.
+                  </p>
+                </div>
+                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                  <span className="testimonial-item-name text-color-high">
+                    Tiana Laurence
+                  </span>
+                  <span className="text-color-low"> / </span>
+                  <span className="testimonial-item-link">
+                    <a href="#0">
+                      Co-Founder of Factom & Author of Blockchain for Dummies.
+                    </a>
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Testimonial.propTypes = propTypes;
+Testimonial.defaultProps = defaultProps;
+
+export default Testimonial;
