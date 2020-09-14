1287ed8715b2be369c89ed2629968ec6fa1be183
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-14 15:56:01 +0200

    remove unnecessary files

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,17 +1,10 @@
 import React, { Component } from "react";
-import Hero from "../Hero/Hero";
-import SecondSection from "../SecondSection/SecondSection";
-import ThirdSection from "../ThirdSection/ThirdSection";
-import Footer from "../Footer/Footer";
 
 class Home extends Component {
   render() {
     return (
       <div>
-        <Hero />
-        <SecondSection />
-        <ThirdSection />
-        <Footer />
+        <h1>Home Landing Page</h1>
       </div>
     );
   }
