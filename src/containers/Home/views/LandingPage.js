9492bd4c4f67f440a4042c7b6f1cf488d393cd61
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-14 21:00:53 +0200

    Add LandingPage component

---
+++ src/containers/Home/views/LandingPage.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import sections
+import Hero from "../components/sections/Hero";
+import FeaturesTiles from "../components/sections/FeaturesTiles";
+import FeaturesSplit from "../components/sections/FeaturesSplit";
+import Testimonial from "../components/sections/Testimonial";
+import Cta from "../components/sections/Cta";
+
+const LandingPage = () => {
+  return (
+    <>
+      <Hero className="illustration-section-01" />
+      <FeaturesTiles />
+      <FeaturesSplit
+        invertMobile
+        topDivider
+        imageFill
+        className="illustration-section-02"
+      />
+      <Cta split />
+      <Testimonial topDivider />
+    </>
+  );
+};
+
+export default LandingPage;
