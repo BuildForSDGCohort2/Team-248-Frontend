010249911d2fe66367ddadc43ca8feaee384b1ef
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-14 20:59:15 +0200

    Add Home component

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,33 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from "react";
+import { Switch } from "react-router-dom";
+import AppRoute from "./utils/AppRoute";
+import ScrollReveal from "./utils/ScrollReveal";
+import LayoutDefault from "./layouts/LayoutDefault";
+import LandingPage from "./views/LandingPage";
 
-class Home extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Home Landing Page</h1>
-      </div>
-    );
-  }
-}
+const Home = () => {
+  const childRef = useRef();
+
+  useEffect(() => {
+    document.body.classList.add("is-loaded");
+    childRef.current.init();
+  }, []);
+
+  return (
+    <ScrollReveal
+      ref={childRef}
+      children={() => (
+        <Switch>
+          <AppRoute
+            exact
+            path="/"
+            component={LandingPage}
+            layout={LayoutDefault}
+          />
+        </Switch>
+      )}
+    />
+  );
+};
 
 export default Home;
