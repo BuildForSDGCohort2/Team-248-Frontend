2ce0a58d9da6bc2be3be47388408b778772065cf
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-09-14 21:45:32 +0200

    Add FeaturesSplit section

---
+++ src/containers/Home/components/sections/FeaturesSplit.js
@@ -0,0 +1,178 @@
+import React from "react";
+import classNames from "classnames";
+import { SectionSplitProps } from "../../utils/SectionProps";
+import SectionHeader from "./partials/SectionHeader";
+import Image from "../elements/Image";
+
+const propTypes = {
+  ...SectionSplitProps.types,
+};
+
+const defaultProps = {
+  ...SectionSplitProps.defaults,
+};
+
+const FeaturesSplit = ({
+  className,
+  topOuterDivider,
+  bottomOuterDivider,
+  topDivider,
+  bottomDivider,
+  hasBgColor,
+  invertColor,
+  invertMobile,
+  invertDesktop,
+  alignTop,
+  imageFill,
+  ...props
+}) => {
+  const outerClasses = classNames(
+    "features-split section",
+    topOuterDivider && "has-top-divider",
+    bottomOuterDivider && "has-bottom-divider",
+    hasBgColor && "has-bg-color",
+    invertColor && "invert-color",
+    className
+  );
+
+  const innerClasses = classNames(
+    "features-split-inner section-inner",
+    topDivider && "has-top-divider",
+    bottomDivider && "has-bottom-divider"
+  );
+
+  const splitClasses = classNames(
+    "split-wrap",
+    invertMobile && "invert-mobile",
+    invertDesktop && "invert-desktop",
+    alignTop && "align-top"
+  );
+
+  const sectionHeader = {
+    title: "BabeJo Workflow",
+    paragraph:
+      "Blockchains can provide an immutable record of transactions, which makes them useful for applications in situations where business actors may not fully trust each other.",
+  };
+
+  return (
+    <section {...props} className={outerClasses}>
+      <div className="container">
+        <div className={innerClasses}>
+          <SectionHeader data={sectionHeader} className="center-content" />
+          <div className={splitClasses}>
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Blockchain workflow
+                </div>
+                <h3 className="mt-0 mb-12">
+                  Decentralized Network Architecture
+                </h3>
+                <p className="m-0">
+                  A blockchain is a chain of blocks which contain specific
+                  information (database), but in a secure and genuine way that
+                  is grouped together in a network (peer-to-peer). In other
+                  words, blockchain is a combination of computers linked to each
+                  other instead of a central server, meaning that the whole
+                  network is decentralized.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-01.png")}
+                  alt="Features split 01"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-right"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Blockchain workflow
+                </div>
+                <h3 className="mt-0 mb-12">P2P vs Client-Server</h3>
+                <p className="m-0">
+                  The traditional architecture of the World Wide Web uses a
+                  client-server network. In this case, the server keeps all the
+                  required information in one place so that it is easy to
+                  update, In the case of the distributed network of blockchain
+                  architecture, each participant within the network maintains,
+                  approves, and updates new entries. The system is controlled
+                  not only by separate individuals, but by everyone within the
+                  blockchain network.
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-02.png")}
+                  alt="Features split 02"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+
+            <div className="split-item">
+              <div
+                className="split-item-content center-content-mobile reveal-from-left"
+                data-reveal-container=".split-item"
+              >
+                <div className="text-xxs text-color-primary fw-600 tt-u mb-8">
+                  Blockchain workflow
+                </div>
+                <h3 className="mt-0 mb-12">Blocks Structure</h3>
+                <p className="m-0">
+                  Logically, the first block does not contain the pointer since
+                  this one is the first in a chain. At the same time, there is
+                  potentially going to be a final block within the blockchain
+                  database that has a pointer with no value. Basically, the
+                  following blockchain sequence diagram is a connected list of
+                  records:
+                </p>
+              </div>
+              <div
+                className={classNames(
+                  "split-item-image center-content-mobile reveal-from-bottom",
+                  imageFill && "split-item-image-fill"
+                )}
+                data-reveal-container=".split-item"
+              >
+                <Image
+                  src={require("./../../assets/images/features-split-image-03.png")}
+                  alt="Features split 03"
+                  width={528}
+                  height={396}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FeaturesSplit.propTypes = propTypes;
+FeaturesSplit.defaultProps = defaultProps;
+
+export default FeaturesSplit;
