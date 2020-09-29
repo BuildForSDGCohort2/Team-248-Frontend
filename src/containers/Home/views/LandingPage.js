2b3010dfb71ec443eef99ad5a5c18a87e92b63f5
AbdullahMuhammed <abdo56668@gmail.com>
2020-09-29 09:48:54 +0200

    add acceptance qualifications to landing page

--- src/containers/Home/views/LandingPage.js
+++ src/containers/Home/views/LandingPage.js
@@ -4,6 +4,7 @@ import Hero from "../components/sections/Hero";
 import FeaturesTiles from "../components/sections/FeaturesTiles";
 import FeaturesSplit from "../components/sections/FeaturesSplit";
 import Testimonial from "../components/sections/Testimonial";
+import { AcceptanceQualifictions } from "../components/sections/AcceptanceQualifications";
 //import Cta from "../components/sections/Cta";
 
 const LandingPage = () => {
@@ -17,6 +18,7 @@ const LandingPage = () => {
         imageFill
         className="illustration-section-02"
       />
+      <AcceptanceQualifictions />
       <Testimonial topDivider />
     </>
   );
