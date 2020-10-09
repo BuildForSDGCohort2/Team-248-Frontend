d0a35aab0d8c7103d4b6214cc6d9a37fdb562d61
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-10-09 13:06:45 +0200

    Fix Netlify build

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,11 +1,9 @@
 import React, { Component } from "react";
 import SecondSection from "../SecondSection/SecondSection";
 import ThirdSection from "../ThirdSection/ThirdSection";
-import Footer from "../Footer/Footer";
 import IndexHeader from "../Headers/IndexHeader";
 import IndexNavbar from "../Navbars/IndexNavbar";
 import DemoFooters from "../Footers/DemoFooter";
-
 
 class Home extends Component {
   render() {
